9bac8f81bcb1bc744d7a413cc2c58520674f0a27
BHISM <61289719+bhism@users.noreply.github.com>
2023-12-17 18:31:04 +0530

    Update main.dart

--- flutter/main.dart
+++ flutter/main.dart
@@ -1,11 +1,7 @@
-// import 'dart:js_interop';
-
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
-// import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -26,55 +22,31 @@ class MyTabs extends StatefulWidget {
 }
 
 class _MyTabsState extends State<MyTabs> {
-  // Data variables for each tab
   var tab1Data;
   var tab2Data;
-  var price;
-
-  // Timer to fetch data every second
-  Timer? timer;
-
-  // API endpoint
+  double price = 0.0;
+  int quantity = 0;
+  double totalInvestment = 0.0;
+  double profitOrLoss = 0.0;
+  double totalValue = 0.0;
+  Color textColor = Colors.green;
   final String apiUrl = 'http://10.0.2.2:5000/get_accumulated_data';
 
   @override
   void initState() {
     super.initState();
-    // Start fetching data on app initialization
     fetchData();
-    // Schedule data fetching every second
-    timer = Timer.periodic(Duration(seconds: 5), (Timer t) {
+    Timer.periodic(Duration(seconds: 1), (Timer t) {
       fetchData();
     });
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the timer when the widget is disposed
-    if (timer != null) {
-      timer!.cancel();
-    }
-    super.dispose();
   }
 
   Future<void> fetchData() async {
     try {
       final response = await http.get(Uri.parse(apiUrl));
       if (response.statusCode == 200) {
-        // final data = json.decode(response.body);
         final data = json.decode(response.body);
-        // var k =
-        print(">>>>>>>>>>>>>>>>>>>>>");
-        // print(k[0]["feeds"]["NSE_EQ|INE002A01018"]["ff"]["marketFF"]["ltpc"]
-        //     ["ltp"]);
-        print(data[0]["feeds"]["NSE_EQ|INE002A01018"]["ff"]["marketFF"]["ltpc"]
-            ["ltp"]);
-        print(">>>>>>>>>>>>>>>>>>>>>");
-
         setState(() {
-          // Clear previous data and update with new data
-          // tab1Data = {};
-          // tab2Data = {};
           price = data[0]["feeds"]["NSE_EQ|INE002A01018"]["ff"]["marketFF"]
               ["ltpc"]["ltp"];
           tab1Data = data;
@@ -88,6 +60,23 @@ class _MyTabsState extends State<MyTabs> {
     }
   }
 
+  void buyStock(int quantity) {
+    // Calculate the total investment
+    setState(() {
+      totalInvestment = price * quantity;
+    });
+  }
+
+  double calculateProfitOrLoss(int quantity) {
+    // Calculate profit or loss based on the current price and quantity
+    var ff;
+    setState(() {
+      totalValue = price * quantity;
+      ff = totalValue - totalInvestment;
+    });
+    return ff;
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -99,9 +88,15 @@ class _MyTabsState extends State<MyTabs> {
               onPressed: () {
                 setState(() {
                   price = 0;
+                  totalInvestment = 0;
+                  textColor = Colors.green;
+                  // profitOrLoss = 0;
+                  quantity = 0;
+                  totalValue = 0;
+                  profitOrLoss = calculateProfitOrLoss(quantity);
                 });
               },
-              child: Text("sdfsd"),
+              child: Text("Reset"),
             ),
           ],
           bottom: TabBar(
@@ -114,35 +109,67 @@ class _MyTabsState extends State<MyTabs> {
         ),
         body: TabBarView(
           children: [
-            buildTab(price),
-            buildTab(price),
+            buildTab(price, quantity),
+            buildTab(price, quantity),
           ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Buy Stock"),
+                  content: Column(
+                    children: <Widget>[
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration:
+                            InputDecoration(labelText: "Enter Quantity"),
+                        onChanged: (value) {
+                          setState(() {
+                            quantity = int.tryParse(value) ?? 0;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  actions: <Widget>[
+                    Text("Total Investment: $totalInvestment"),
+                    ElevatedButton(
+                      child: Text("Buy"),
+                      onPressed: () {
+                        buyStock(quantity);
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
   }
 
-  // Widget buildTab(var data) {
-  //   return ListView.builder(
-  //     itemCount: data.length,
-  //     itemBuilder: (context, index) {
-  //       return ListTile(
-  //         title: Text(data[index]),
-  //       );
-  //     },
-  //   );
-  // }
-
-  Widget buildTab(var data) {
-    print("????????????");
-    print(data);
-    print("????????????");
+  Widget buildTab(double price, int quantity) {
+    setState(() {
+      profitOrLoss = calculateProfitOrLoss(quantity);
+    });
+    textColor = profitOrLoss >= 0 ? Colors.green : Colors.red;
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Text("Reliance  : " + data.toString()),
+        Text("Reliance Price: $price"),
+        Text("total Investment: $totalInvestment"),
+        Text(
+          "Profit or Loss: $profitOrLoss",
+          style: TextStyle(color: textColor),
+        ),
       ],
     );
   }
